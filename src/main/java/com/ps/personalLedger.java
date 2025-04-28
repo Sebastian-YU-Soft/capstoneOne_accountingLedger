70a4e94c7ba229a0c946f2bc4dceb369d7c4cf34
Sebastian <sebjerume@gmail.com>
2025-04-28 12:15:20 -0400

    Trying to addTransaction into a boolean

--- src/main/java/com/ps/personalLedger.java
+++ src/main/java/com/ps/personalLedger.java
@@ -1,7 +1,7 @@
 package com.ps;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class personalLedger {
@@ -34,6 +34,46 @@ public class personalLedger {
             System.out.println("4. Exit");
             System.out.println("Please enter your choice:");
             String choice = scanner.nextLine().trim().toUpperCase();
+
+            switch (choice){
+                case "1":
+                    addTransaction(true);
+                    break;
+                case "2":
+                    addTransaction(false);
+                    break;
+                case "3":
+                    ledgeMenu();
+                    break;
+                case "4":
+                    running = false;
+                    System.out.println("Exiting application. Have a Good Day!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try Again.");
+            }
+        }
+    }
+
+    private static void addTransaction(boolean isDeposit){
+        try {
+            (FileWriter fw = new FileWriter(CSV_FILE,true);
+            PrintWriter pw = new PrintWriter(fw)) {
+
+                System.out.print("Enter vendor/description");
+                String description = scanner.nextLine();
+
+                System.out.print("Enter amount:");
+                double amount = Double.parseDouble(scanner.nextLine());
+
+                if (isDeposit){
+                    amount = -amount;
+                }
+
+                String date = LocalDate.now().toString();
+                pw.println(date + "|" + description + "|" + amount);
+                System.out.println("Transaction saved!");
+            }
         }
     }
 }
