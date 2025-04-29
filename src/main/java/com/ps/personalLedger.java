3a46b58e6c25f460c8141c0272c573bce7e40d70
Sebastian <sebjerume@gmail.com>
2025-04-29 10:37:36 -0400

    finished the try/catch method now making the ledger menu

--- src/main/java/com/ps/personalLedger.java
+++ src/main/java/com/ps/personalLedger.java
@@ -1,6 +1,7 @@
 package com.ps;
 
 import java.io.*;
+import java.nio.channels.ScatteringByteChannel;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -13,26 +14,25 @@ public class personalLedger {
         mainMenu();
     }
 
-    private static void ensureCSVFileExists(){
+    private static void  ensureCSVFileExists(){
         try {
             File file = new File(CSV_FILE);
-            if (!file.exists()) {
+            if (!file.exists()){
                 file.createNewFile();
             }
         } catch (IOException e) {
-            System.out.println("Creating A CSV file:"+ e.getMessage());
+            System.out.println("Error creating CSV file:" + e.getMessage());
         }
     }
-
     private static void mainMenu(){
         boolean running = true;
         while (running){
             System.out.println("Home Screen");
             System.out.println("1. Add Deposit");
-            System.out.println("2. Make Payment (DEBIT)");
+            System.out.println("2. Make Payment (Debit)");
             System.out.println("3. Ledger");
             System.out.println("4. Exit");
-            System.out.println("Please enter your choice:");
+            System.out.println("Enter your choice:");
             String choice = scanner.nextLine().trim().toUpperCase();
 
             switch (choice){
@@ -43,37 +43,62 @@ public class personalLedger {
                     addTransaction(false);
                     break;
                 case "3":
-                    ledgeMenu();
+                    ledgerMenu();
                     break;
                 case "4":
                     running = false;
-                    System.out.println("Exiting application. Have a Good Day!");
+                    System.out.println("Exiting application. Goodbye");
                     break;
                 default:
-                    System.out.println("Invalid choice. Try Again.");
+                    System.out.println("invalid option. Try again.");
             }
         }
     }
 
     private static void addTransaction(boolean isDeposit){
         try {
-            (FileWriter fw = new FileWriter(CSV_FILE,true);
-            PrintWriter pw = new PrintWriter(fw)) {
+            (FileWriter fw = new FileWriter(CSV_FILE, true);
+            PrintWriter pw = new PrintWriter(fw)){
 
-                System.out.print("Enter vendor/description");
+                System.out.print("Enter vendor/description:");
                 String description = scanner.nextLine();
 
                 System.out.print("Enter amount:");
                 double amount = Double.parseDouble(scanner.nextLine());
 
-                if (isDeposit){
+                if (!isDeposit) {
                     amount = -amount;
                 }
 
                 String date = LocalDate.now().toString();
                 pw.println(date + "|" + description + "|" + amount);
                 System.out.println("Transaction saved!");
+
+            }catch(IOException e){
+                System.out.println("Error saving transaction:" + e.getMessage());
             }
+        } finally {(NumberFormatException e){
+            System.out.println("Invalid amount entered.");
+        }
+
+        }
+    }
+
+    private static void ledgerMenu(){
+        boolean inLedger = true;
+        while (inLedger){
+            System.out.println("Ledger Screen");
+            System.out.println("1. All Entries");
+            System.out.println("2. Deposits Only");
+            System.out.println("3. Payments Only");
+            System.out.println("4. Reports");
+            System.out.println("5. Home");
+            System.out.println("Enter your choice:");
+            String choice = scanner.nextLine().trim().toUpperCase();
+
+
         }
     }
 }
+
+
